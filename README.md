e6826942163896fdeb8af9298ed7b5e6031d0a7e
mehdi <mehdi.shokohifar@gmail.com>
2020-07-31 00:15:13 +0430

    init Mongo Native Driver Connection Pool

---
+++ README.md
@@ -0,0 +1,27 @@
+### Mongo Turbo Connection Pool
+###### Round Robin Connection Pool for Load Balancing Mongo Connection 
+
+***How Does It Work***
+
+Connection Pool In This Package Operates In Two Level .
+L1 Pool is Handled By Native Driver MongoDb  ‌, Besides That L2 Pool is Array Of Connections ,which  are  available on round robin basis. 
+
+
+##### How to use
+```js
+let client = require("mongo-turbo-connection")
+let mongo =new client("DBNAME","IP/Mongo_Server","User","Password",Mongo_PORT,L1POOL,L2POOL);
+
+(async ()=>{
+  try {
+    for(let i= 0 ;i<10;i++) {
+      let db = await mongo.getConnection();
+      const result = await db.collection("user").findOne({_id: mongo.objectID('5ef9a136fb25d11a39ba3f93')});
+      console.log(result)
+    }
+  }catch (e) {
+    console.log(e)
+  }
+})()
+
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+var MongoClient = require('mongodb').MongoClient
+let ObjectID = require('mongodb').ObjectID
+class MongoConnection
+{
+  static mongo_client = []
+  static ind = 0
+  constructor (db, host, user, password, port, L1_Pool = 10 ,L2_Pool=10) {
+    this.db=db
+    this.l1pool = L1_Pool
+    this.l2pool = L2_Pool
+    this.url = `mongodb://${user}:${password}@${host}:${port}`;
+
+  }
+
+
+  index(){
+  if(MongoConnection.ind===this.l2pool) MongoConnection.ind=0
+  return MongoConnection.ind++
+
+}
+  newConnection () {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(this.url, {
+        useUnifiedTopology: true, poolSize: this.l1pool
+      }, function (err, client) {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(client)
+        }
+      })
+    })
+  }
+
+ async  getConnection(){
+    try {
+      // console.log("Length Of Ext. Connection Pool : "+MongoConnection.mongo_client.length)
+
+      if (MongoConnection.mongo_client.length < this.l2pool) {
+        let cli = await this.newConnection()
+        MongoConnection.mongo_client.push(cli)
+
+      }
+      let ind = this.index()
+      // console.log("index oF Mongo Connection Gate: "+ind)
+      let client = MongoConnection.mongo_client[ind];
+      if (client.isConnected())
+      return client.db(this.db)
+      else {
+        delete MongoConnection.mongo_client[ind]
+        return this.getConnection()
+      }
+    }catch (e) {
+      console.log(e)
+    }
+  }
+
+  objectID(id){
+    return new ObjectID(id)
+  }
+
+
+}
+
+
+module.exports = MongoConnection
